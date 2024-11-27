b3dd340755df6a23a8bc921713bb9cd682279fd5
unknown <yc0519165>
2024-11-27 10:28:30 +0530

    save

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ const address = document.getElementById("city");
 const humanity = document.getElementById("humanity");
 const speed = document.getElementById("speed");
 
-const api =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/pune/2024-11-26?key=MA85H3QBHK3FGLU79LG3XRJM7";
+//"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/pune/2024-11-27?key=UYHMPVGP4MTZKXZUHM65LDGFF"
 
 async function checkWeather() {
   const city = input.value;
-  document.getElementById("input").value = "";
+  const date = new Date();
 
+  const year = date.getFullYear();
+  const day = date.getDay();
+  document.getElementById("input").value = "";
   const data = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/2024-11-26?key=MA85H3QBHK3FGLU79LG3XRJM7`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${year}-11-27?key=UYHMPVGP4MTZKXZUHM65LDGFF`
   );
   const res = await data.json();
   console.log(res);
+  // const tempa = res.Date[0].temp
   deg.innerHTML = `${res.currentConditions.temp}\u00B0C`;
-
   humanity.innerHTML = `${res.currentConditions.humidity} %`;
   address.innerHTML = res.address;
   speed.innerHTML = `${res.currentConditions.windspeed} Km/h`;
